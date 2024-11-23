cfdfcab12576e232e346221950794554b857fb27
Rohith Kumar Reddy <111877972+RohithReddy-1402@users.noreply.github.com>
2024-11-24 04:56:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import "./index.css"
+const root = createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+root.render(
+<Auth0Provider
+    domain="chatapp-react.us.auth0.com"
+    clientId="ZuT3F1o4fWkmuBeQjq1ntJC7Cm7Nl6Ff"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>,
+);
